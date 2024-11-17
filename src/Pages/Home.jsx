af17b9adcb23c458c85a9414fe9683297602934b
AnkittChauhan <ankittrajput.4@gmail.com>
2024-11-17 20:22:45 +0530

    Added Line on What we do Video

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1030,6 +1030,8 @@ const WhatWeDoSection = () => {
     <section className="flex lg:h-screen overflow-hidden flex-col bg-zinc-800">
       {!showSection && (
         <div className={`flex relative flex-col px-20 pt-16 w-full min-h-[1126px] max-md:px-5 max-md:py-24 max-md:max-w-full transition-opacity duration-500 ${fadeOut ? 'opacity-0' : 'opacity-100'}`}>
+         
+         <h1 className="z-50 text-white font-bold absolute inset-0 my-[50vh] flex  justify-center text-4xl underline">India to 24 Countries</h1>
           <video
             ref={videoRef}
             onEnded={handleVideoEnd}
