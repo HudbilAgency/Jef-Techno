f08435357200006742174b36fa5dd2d58760a672
Lalit Kumar <lalitchauhan1517@gmail.com>
2024-11-21 21:16:57 +0530

    updated contact component

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -11,6 +11,11 @@ const ContactUs = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const toggleConfirmation = () => {
+    setShowConfirmation(!showConfirmation);
+  };
 
   useEffect(() => {
 
@@ -65,9 +70,26 @@ const ContactUs = () => {
       mobile,
     };
 
+    toggleConfirmation();
+    setTimeout(() => {
+      const element = document.querySelector('.thank-you');
+      const elementRect = element.getBoundingClientRect();
+      const elementTop = elementRect.top + window.scrollY;
+      const centerPosition = elementTop - (window.innerHeight / 2) + (elementRect.height / 2);
+
+      window.scrollTo({
+        top: centerPosition,
+        behavior: 'smooth'
+      });
+    }, 0);
+
+    setName("");
+    setEmail("");
+    setMobile("");
+
     try {
       const response = await axios.post(
-        "https://jef-server.onrender.com/reach-us-hudbil",
+        "http://localhost:3000/contact-us-jef",
         JSON.stringify(contactformData),
         {
           headers: {
@@ -79,9 +101,6 @@ const ContactUs = () => {
     } catch (error) {
       console.error("Error sending data to server:", error);
     }
-    setName("");
-    setEmail("");
-    setMobile("");
   };
 
   return (
@@ -97,54 +116,61 @@ const ContactUs = () => {
           <p className="self-stretch Y-axis-text mt-4 text-sm lg:text-base font-normal leading-none text-white max-md:max-w-full">
             Get in touch with us for any business enquiry.
           </p>
-          <form id="userDetailsForm"
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmitForm();
-            }} className="w-full Y-axis-text mt-12 max-md:mt-10">
-            {/* {inputFields.map((field, index) => (
+          {showConfirmation ? (
+            <div className='w-full flex flex-col items-start gap-12 thank-you mt-16 text-xl Y-axis-text lg:text-2xl font-semibold text-red-700 uppercase tracking-[2px]'>
+              <div className='leading-tight'>Thank You…!! </div>
+              <div className='leading-tight'>Our team has received your information, will get in touch with you within the next 24 hours to discuss how we can help bring your vision to life.</div>
+            </div>
+          ) : (
+            <form id="userDetailsForm"
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmitForm();
+              }} className="w-full Y-axis-text mt-12 max-md:mt-10">
+              {/* {inputFields.map((field, index) => (
             <FormInput key={index} label={field.label} type={field.type} />
           ))} */}
-            <div className="mb-6">
-              <label className="sr-only">Name</label>
-              <input
-                type="text"
-                name="text"
-                placeholder={'Name'}
-                className="overflow-hidden Y-axis-text px-5 py-3 w-full font-medium text-black max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
-                required
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <label className="sr-only">Email</label>
-              <input
-                type="email"
-                name="email"
-                placeholder={'Email'}
-                className="overflow-hidden Y-axis-text px-5 py-3 w-full font-medium text-black max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-6">
-              <label className="sr-only">Mobile Number</label>
-              <input
-                type="number"
-                name="number"
-                placeholder={'Mobile Number'}
-                className="overflow-hidden Y-axis-text px-5 font-medium text-black py-3 w-full max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
-                required
-                onChange={(e) => setMobile(e.target.value)}
-              />
-            </div>
-            <button
-              form="userDetailsForm" type="submit"
-              className="overflow-hidden Y-axis-text px-16 py-3 mt-2 max-w-full text-xl font-semibold text-white whitespace-nowrap bg-red-700 rounded-3xl w-[380px] max-md:px-5"
-            >
-              Submit
-            </button>
-          </form>
+              <div className="mb-6">
+                <label className="sr-only">Name</label>
+                <input
+                  type="text"
+                  name="text"
+                  placeholder={'Name'}
+                  className="overflow-hidden Y-axis-text px-5 py-3 w-full font-medium text-black max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-6">
+                <label className="sr-only">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder={'Email'}
+                  className="overflow-hidden Y-axis-text px-5 py-3 w-full font-medium text-black max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-6">
+                <label className="sr-only">Mobile Number</label>
+                <input
+                  type="number"
+                  name="number"
+                  placeholder={'Mobile Number'}
+                  className="overflow-hidden Y-axis-text px-5 font-medium text-black py-3 w-full max-w-[380px] bg-white rounded-3xl border border-solid border-neutral-200"
+                  required
+                  onChange={(e) => setMobile(e.target.value)}
+                />
+              </div>
+              <button
+                form="userDetailsForm" type="submit"
+                className="overflow-hidden Y-axis-text px-16 py-3 mt-2 max-w-full text-xl font-semibold text-white whitespace-nowrap bg-red-700 rounded-3xl w-[380px] max-md:px-5"
+              >
+                Submit
+              </button>
+            </form>
+          )}
         </div>
       </div>
     </section>
